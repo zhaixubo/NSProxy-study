6fefa97b108212f916077ccb9f61e7dbd682b139
zhaixubo <113840032+zhaixubo@users.noreply.github.com>
2023-05-09 13:19:42 +0800

    Initial Commit

---
+++ NSProxy测试/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NSProxy测试
+//
+//  Created by 翟旭博 on 2023/5/9.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
